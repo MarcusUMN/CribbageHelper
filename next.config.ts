d56d79b2969e2e0ba8717cfba7b52779e06d969b
Marcus <marcustaugner@outlook.com>
2025-07-07 23:14:07 -0500

    add subdomain

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',  
+  basePath: '/CribbageHelper',
+  assetPrefix: '/CribbageHelper',
   eslint: {
     ignoreDuringBuilds: true,
   },
